679d62cf87ae63df7ac410d929fb38a169095166
lutherwaves <git config --global user.email>
2024-12-27 19:58:40 +0200

    chore: Adjust const

--- src/utils/indexer/consts.js
+++ src/utils/indexer/consts.js
@@ -1,11 +1,11 @@
 export const ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
-export const ALGOLIA_INDEX_NAME_SUFFIX = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME;
+export const ALGOLIA_INDEX_NAME_SUFFIX = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || 'debugmydinner';
 export const ALGOLIA_SEARCH_API_KEY = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY;
 
 export const ALGOLIA_ADMIN_API_KEY = process.env.ALGOLIA_ADMIN_API_KEY;
 export const ENV_NAME = process.env.NODE_ENV || 'development';
+export const ALGOLIA_INDEX_NAME = process.env.NEXT_ALGOLIA_INDEX_NAME || `${ENV_NAME}_${ALGOLIA_INDEX_NAME_SUFFIX}`;
 
 export function buildIndexName() {
-    const indexName = process.env.NEXT_ALGOLIA_INDEX_NAME || `${ENV_NAME}_${ALGOLIA_INDEX_NAME_SUFFIX}`;
-    return indexName;
+    return ALGOLIA_INDEX_NAME;
 }
